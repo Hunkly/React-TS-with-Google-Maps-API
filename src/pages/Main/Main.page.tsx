4c2ee2a877f18615d26efdb96b1f650fd6fff52d
viktors <vityasapozhnik1998@gmail.com>
2019-06-18 19:37:23 +0300

    Little fixes before sending

--- src/pages/Main/Main.page.tsx
+++ src/pages/Main/Main.page.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Map from '../../components/Map/Map';
 import MainPageStyled from './Main.styled';
 import Header from "../../components/Header";
-import HotelMap from '../../components/Map/hospital';
 
 export default function Main(){
     return(
         <MainPageStyled>
             <Header children="Main page"/>
             <div className="main-page__map">
-                <Map/>
+                <Map />
             </div>
         </MainPageStyled>
     )
